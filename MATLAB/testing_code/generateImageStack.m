38b2d743947458f566d7db327b45e32741bf8fe0
Dan Sakaguchi <dsakaguc@stanford.edu>
2013-12-09 22:09:16 -0800

    generalized to allow just directory input

---
+++ MATLAB/testing_code/generateImageStack.m
@@ -0,0 +1,19 @@
+function imageStack = generateImageStack(directory, ext) 
+	files = dir(directory);
+
+	numNames = [];
+    for i = 1:length(files)
+    	if ~isempty(findstr(files(i).name,ext))
+    		image = imread(files(i).name);
+    		if size(image,3) == 3
+    			bwImage = rgb2gray(image);
+    			image = bwImage(:,:,1);
+    		end
+        	imageStack(:,:,i) = image;
+        	numNames = cat(1,numNames,str2num(regexprep(files(i).name,'[a-zA-Z.]','')));
+        end
+    end
+
+    [~, sortedIndices] = sort(numNames);
+    imageStack = imageStack(:,:,sortedIndices);
+end
